4827227a81cebdcc001caf546f85d8ae4e11db76
Jorge Yanar <jorgeyanar@gmail.com>
2020-07-31 00:28:33 -0400

    boids sim working

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,27 @@ use std::time::{SystemTime,UNIX_EPOCH};
 use std::path::Path;
 use piston_window::*;
 
-
-
-const NBOIDS: usize = 200;    // Number of boids
-const NPREDS: usize = 0;     // Number of predator boids
-const MAXSPEED: f64 = 1.5;    // Max speed of boids
-const MAXFORCE: f64 = 1.0;    // Max force to apply
-const MAXDIST: f64  = 5.0;  // Max distance to apply rules
-const MINSEP: f64   = 3.0;
-
-const ALIGN_WEIGHT: f64 = 1.0;
-const SEPAR_WEIGHT: f64 = 1.0;
-const COHSN_WEIGHT: f64 = 1.0;
-
-const WINDOW_HEIGHT: usize = 2024;
-const WINDOW_WIDTH: usize  = 1024;
-
-// Piston Window stuff
-const WINDOW_SIZE: u32 = 1024;
-//const GFX_CONTEXT_OFFSET: f64 = (WINDOW_SIZE / 2) as f64;
+// Piston settings
+const WINDOW_SIZE: u32 = 800;
 const GFX_CONTEXT_OFFSET: f64 = 0.0 as f64;
-const MILLIS_PER_FRAME: u128 = 1;
+const MILLIS_PER_FRAME: u128 = 10;
 const BLACK: [f32;4] = [0.0, 0.0, 0.0, 1.0];
 const WHITE: [f32;4] = [1.0; 4];
 const SQUARE_SIZE: f64 = 2.0;
+
+// Simulation settings
+const NBOIDS: usize = 300;   // Number of boids
+const NPREDS: usize = 0;     // Number of predator boids
+const MAXSPEED: f64 = 3.5;   // Max speed of boids
+const MAXFORCE: f64 = 1.0;   // Max force to apply
+const MAXDIST: f64  = 25.0;  // Max distance to apply rules
+const MINSEP: f64   = 10.0;
+
+const ALIGNMENT_WEIGHT: f64  = 1.0;
+const SEPARATION_WEIGHT: f64 = 1.0;
+const COHESION_WEIGHT: f64   = 1.0;
+const OBSTACLE_WEIGHT: f64   = 1.0;
+
 
 #[derive(Debug)]
 struct Vector {
@@ -101,7 +98,9 @@ impl Boid {
                                             let sum = (sum.0 + v.0, sum.1 + v.1);
                                             sum
                                         });
-        let alignment = Vector {x: alignment.0 / 8.0, y: alignment.1 / 8.0};
+        let mut alignment = Vector { x: alignment.0 , y: alignment.1 };
+        // alignment.multscalar(1.0/8.0);
+        alignment.multscalar(1.0);
         alignment
     }
 
@@ -118,49 +117,63 @@ impl Boid {
                                              let sum = (sum.0 + l.0, sum.1 + l.1);
                                              sum
                                          });
-        let separation: Vector = Vector { x: separation.0 / 10.0, y: separation.1 / 10.0};
+        let mut separation: Vector = Vector { x: separation.0, y: separation.1 };
+        // separation.multscalar(1.0/10.0);
+        separation.multscalar(1.0);
         separation
     }
 
     /// Boids move towards the center of mass
     fn compute_cohesion(&self, boids: &[Boid]) -> Vector {
-        let n_boids = boids.len() as f64;
         // To compute the boids center of mass, we filter out boids
         // which are further away than MAXDIST, and compute their average
         // (x,y) position
-        let locs_sum: (f64,f64) = boids.iter()
-                                       .filter(|b| b.id != self.id)
-                                       .map(|b| (b.location.x, b.location.y))
-                                       .filter(|l| (l.0 - self.location.x).abs() < MAXDIST &&
-                                                   (l.1 - self.location.y).abs() < MAXDIST)
-                                       .fold((0.0, 0.0), |sum, l| {
-                                           let sum = (sum.0 + l.0 , sum.1 + l.1);
-                                           sum
-                                       });
-        let centroid: (f64,f64) = (locs_sum.0/(n_boids - 1.0), locs_sum.1/(n_boids - 1.0));
-        // Vector designating direction from self.location to the centroid
-        let cohesion: Vector = Vector { x: (centroid.0 - self.location.x) / 100.0 ,
-                                        y: (centroid.1 - self.location.y) / 100.0 };
+        let mut centroid = [0.0, 0.0];
+        let mut nearby = 0;
+        for i in 0 .. boids.len() {
+            if (boids[i].location.x - self.location.x).abs() < MAXDIST &&
+               (boids[i].location.y - self.location.y).abs() < MAXDIST &&
+               (boids[i].id != self.id) {
+                centroid[0] = centroid[0] + boids[i].location.x;
+                centroid[1] = centroid[1] + boids[i].location.y;
+                nearby = nearby + 1;
+            }
+        }
+        centroid[0] = centroid[0] / nearby as f64;
+        centroid[1] = centroid[1] / nearby as f64;
+
+        let mut cohesion = Vector { x: centroid[0] - self.location.x,
+                                    y: centroid[1] - self.location.y };
+        // cohesion.multscalar(1.0 / 100.0);
+        cohesion.multscalar(1.0);
         cohesion
+
+        // let centroid: Vector = boids.iter()
+        //                             .filter(|b| b.id != self.id && 
+        //                                     (b.location.x - self.location.x).abs() < MAXDIST &&
+        //                                     (b.location.y - self.location.y).abs() < MAXDIST)
+        //                             .map(|b| b.location)
+        //                             .fold(Vector{x:0.0,y:0.0}, |sum,l| {
+        //                                 let sum = Vector { x: sum.x + l.x , y: sum.y + l.y };
+        //                                 sum
+        //                             });
     }
 
     /// Boids are repelled from bounds and obstacles
     fn compute_obstacle(&self) -> Vector {
         let mut dir: (f64,f64) = (0.0,0.0);
         if self.location.x < MINSEP {
-            // dir.0 = 100.0 / self.location.x;
+            // dir.0 = 100.0 / (self.location.x);
+            // dir.0 = 10.0;
             dir.0 = 10.0;
         }
         if self.location.y < MINSEP {
-            // dir.1 = 100.0 / self.location.y;
             dir.1 = 10.0;
         }
-        if self.location.x > (WINDOW_WIDTH as f64 - MINSEP) {
-            // dir.0 = -1.0 * (100.0 / (WINDOW_WIDTH as f64 - MINSEP));
+        if self.location.x > (WINDOW_SIZE as f64 - MINSEP) {
             dir.0 = -10.0;
         }
-        if self.location.y > (WINDOW_HEIGHT as f64 - MINSEP) {
-            // dir.1 = -1.0 * (100.0 / (WINDOW_HEIGHT as f64 - MINSEP));
+        if self.location.y > (WINDOW_SIZE as f64 - MINSEP) {
             dir.1 = -10.0;
         }
 
@@ -173,7 +186,7 @@ impl Boid {
     fn update_velocity(&mut self, v: &Vector) {
         self.velocity.addvec(v);
     }
-    fn take_step(&mut self) {
+    fn update_position(&mut self) {
         self.location.addvec(&self.velocity);
     }
     fn limit_speed(&mut self) {
@@ -197,8 +210,8 @@ impl Flock {
                 id: i,
                 predator: false,
                 maxspeed: MAXSPEED, maxforce: MAXFORCE,
-                location: Vector {x: rng.gen_range(0.0, 1000.0),
-                                  y: rng.gen_range(0.0, 20.0)},
+                location: Vector {x: rng.gen_range(0.0, WINDOW_SIZE as f64),
+                                  y: rng.gen_range(0.0, WINDOW_SIZE as f64)},
                 velocity: Vector {x: 1.0, y: 0.0},
                 acceleration: Vector {x: 0.0, y: 0.0},
             });
@@ -220,10 +233,10 @@ impl Flock {
             let mut algn = self.flock[i].compute_alignment(&self.flock);
             let mut sepr = self.flock[i].compute_separation(&self.flock);
             let mut cohn = self.flock[i].compute_cohesion(&self.flock);
-            algn.multscalar(ALIGN_WEIGHT);
-            sepr.multscalar(SEPAR_WEIGHT);
-            cohn.multscalar(COHSN_WEIGHT);
             let obst = self.flock[i].compute_obstacle();
+            algn.multscalar(ALIGNMENT_WEIGHT);
+            sepr.multscalar(SEPARATION_WEIGHT);
+            cohn.multscalar(COHESION_WEIGHT);
             let mut sum = Vector{x: 0.0, y: 0.0};
             sum.addvec(&sepr);
             sum.addvec(&algn);
@@ -231,22 +244,15 @@ impl Flock {
             sum.addvec(&obst);
             self.flock[i].update_velocity(&sum);
             self.flock[i].limit_speed();
-            self.flock[i].take_step();
+            self.flock[i].update_position();
         }
     }
 }
 
 
 fn main() {
-    // Generate flock with 10 boids
     let mut f = Flock { flock: vec![] };
     f.generate_flock(NBOIDS, 0);
-
-
-    // let v: Vector = f.flock[0].compute_cohesion(&f.flock);
-    // let v: Vector = f.flock[0].compute_separation(&f.flock);
-    // let v: Vector = f.flock[0].compute_alignment(&f.flock);
-    // println!("{:?}", v);
 
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = WindowSettings::new("Boids", [WINDOW_SIZE; 2])
